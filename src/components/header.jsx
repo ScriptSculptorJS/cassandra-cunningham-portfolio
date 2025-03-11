51e713ca3ce2814614c25632fe45bf1d7c41432d
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-03-11 14:43:57 -0700

    Created Header component and styled it

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+
+  return(
+    <header>
+      <p class='name'>Cassandra Cunningham</p>
+      <div class='nav-container'>
+        <p class='projects'>Projects</p>
+        <p class='resume'>Resume</p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
