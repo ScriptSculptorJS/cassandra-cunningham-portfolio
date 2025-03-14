fa6c29022f4ea0b6162ef6ea4ca22e95ab03840a
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-03-14 14:48:49 -0700

    Generated HTML and rendered it for the dropdown menu within the header when screen widths get smaller. Made it interactive.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,34 @@
 import { Link } from 'react-router-dom';
+import menu from '../assets/hamburger-menu.png';
 
 function Header() {
+
+  function menuVisible() {
+    const dropdownMenuElement = document.querySelector('.js-dropdown-menu');
+    const menuIconElement = document.querySelector('.js-menu-icon');
+
+    if (dropdownMenuElement.classList.contains('visible')) {
+      dropdownMenuElement.classList.remove('visible')
+
+    } else {
+      dropdownMenuElement.classList.add('visible');
+
+      window.addEventListener('click', (event) => {
+    
+        if (!dropdownMenuElement.contains(event.target) && !menuIconElement.contains(event.target)) {
+          dropdownMenuElement.classList.remove('visible');
+        }
+      });
+    }
+  };
+
+  function menuItemSelected() {
+    const dropdownMenuElement = document.querySelector('.js-dropdown-menu');
+
+    if (dropdownMenuElement.classList.contains('visible')) {
+      dropdownMenuElement.classList.remove('visible')
+    } 
+  };
 
   return(
     <header>
@@ -8,6 +36,14 @@ function Header() {
       <div className='nav-container'>
         <Link to='/about' className='about'>About</Link>
         <Link to='/projects' className='projects'>Projects</Link>
+        <img className='menu-icon js-menu-icon' src={menu} onClick={menuVisible}/>
+        <div className='dropdown-menu js-dropdown-menu'>
+          <Link to='/about' className='menu-about' onClick={menuItemSelected}><p>About</p></Link>
+          <Link to='/projects' className='menu-projects' onClick={menuItemSelected}><p>Projects</p></Link>
+          <a href='https://www.linkedin.com/in/cassandra-cunningham-a29347141/' target='_blank' className='menu-linkedin' onClick={menuItemSelected}><p>LinkedIn</p></a>
+          <a href='https://github.com/ScriptSculptorJS' target='_blank' className='menu-github' onClick={menuItemSelected}><p>Github</p></a>
+          <a href='mailto:cm_cunningham27@yahoo.com' target='_blank' className='menu-email' onClick={menuItemSelected}><p>Email</p></a>
+        </div>
       </div>
     </header>
   )
