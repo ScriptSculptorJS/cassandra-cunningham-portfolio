8a39d8e6b3d46232022ea2b195d8641afb7af718
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-04-29 10:31:08 -0700

    Now the other links are no longer working, so removed the repo name from each of those Link elements as the deployed pages may not have updated at the time I tried before.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -34,12 +34,12 @@ function Header() {
     <header>
       <Link to='/' className='name'> Cassandra Cunningham</Link>
       <div className='nav-container'>
-        <Link to='/cassandra-cunningham-portfolio/about' className='about'>About</Link>
-        <Link to='/cassandra-cunningham-portfolio/projects' className='projects'>Projects</Link>
+        <Link to='/about' className='about'>About</Link>
+        <Link to='/projects' className='projects'>Projects</Link>
         <img className='menu-icon js-menu-icon' src={menu} onClick={menuVisible}/>
         <div className='dropdown-menu js-dropdown-menu'>
-          <Link to='/cassandra-cunningham-portfolio/about' className='menu-about' onClick={menuItemSelected}><p>About</p></Link>
-          <Link to='/cassandra-cunningham-portfolio/projects' className='menu-projects' onClick={menuItemSelected}><p>Projects</p></Link>
+          <Link to='/about' className='menu-about' onClick={menuItemSelected}><p>About</p></Link>
+          <Link to='/projects' className='menu-projects' onClick={menuItemSelected}><p>Projects</p></Link>
           <a href='https://www.linkedin.com/in/cassandra-cunningham-a29347141/' target='_blank' className='menu-linkedin' onClick={menuItemSelected}><p>LinkedIn</p></a>
           <a href='https://github.com/ScriptSculptorJS' target='_blank' className='menu-github' onClick={menuItemSelected}><p>Github</p></a>
           <a href='mailto:cm_cunningham27@yahoo.com' target='_blank' className='menu-email' onClick={menuItemSelected}><p>Email</p></a>
