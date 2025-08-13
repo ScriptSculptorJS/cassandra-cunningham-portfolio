f1d02302a7117bdd6a772a0f609a1d7c62f16fe6
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-08-13 12:51:31 -0700

    Added youtube project demonstration video links for projects that are not deployed at this time

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,42 +1,53 @@
 import Project from '../components/project.jsx';
-import image1 from '../../public/assets/instagram-feed-practice.png';
-import image2 from '../../public/assets/lets-go-project.png';
-import image3 from '../../public/assets/Rhody-park.png';
-import image4 from '../../public/assets/Memraze.jpg';
+import image1 from '/assets/instagram-feed-practice.png';
+import image2 from '/assets/lets-go-project.png';
+import image3 from '/assets/Rhody-park.png';
+import image4 from '/assets/Memraze.jpg';
+import image5 from '/assets/annual-report-tracker-and-reminder-app.jpg'
 import React from 'react';
 
 function Projects() {
   let projectComponentArray = [];
+  
 
   const projects = [{
     id: 1,
+    image: image5,
+    name: 'Annual Report Tracker & Reminder App',
+    link: 'https://youtu.be/2fusCLYstFU',
+    skills: 'MERN | Bootstrap | JWT Authentication & Backend Cookies | CRUD',
+    description: 'Full-stack MERN app that helps small businesses track annual report deadlines and rendering reminders, featuring JWT authentication, secure cookies, nested CRUD operations, and a responsive Bootstrap UI.',
+    repoLink: 'https://github.com/ScriptSculptorJS/Annual-Report-Tracker-and-Reminders-App',
+  },{
+    id: 2,
     image: image4,
     name: 'Memraze',
+    link: 'https://youtu.be/3zuAoxVZfMk',
     skills: 'HTML5 | CSS | Bootstrap | Javascript | React | MongoDB | JWT Authentication & Backend Cookies | CRUD',
-    description: 'Figured out how to use JWT for authenticating users logging into the app to begin creating a profile that showcases their interests and hobbies in tabs and each interest/hobby can have posts sorted from most recent to oldest. This is really showcasing CRUD for their interests/hobbies.',
-    repoLink: 'https://github.com/ScriptSculptorJS/Memraze'
+    description: 'Secure MERN app where users create profiles, add interest-based tabs, and share posts with most-recent sorting, featuring JWT authentication and full CRUD operations.',
+    repoLink: 'https://github.com/ScriptSculptorJS/Memraze',
   }, {
-    id: 2,
+    id: 3,
     image: image1,
     name: 'Instagram Feed Practice',
     link: 'https://scriptsculptorjs.github.io/instagram-feed-practice/',
     skills: 'HTML5 | CSS',
-    description: 'Using the skills I acquired from a course on Youtube by SuperSimpleDev to recreate the Instagram feed page directly after logging in, and making it responsive.',
-    repoLink: 'https://github.com/ScriptSculptorJS/instagram-feed-practice' 
+    description: 'Built with only HTML & CSS, replicating Instagram’s post-login feed layout with responsive design techniques inspired by a SuperSimpleDev tutorial.',
+    repoLink: 'https://github.com/ScriptSculptorJS/instagram-feed-practice'
   }, {
-    id: 3,
+    id: 4,
     image: image3,
     name: 'Rhododendron Park Maintenance Company Website',
     link: 'https://rhodypark.com',
     skills: 'WordPress | HTML5',
-    description: "Utilizing WordPress to create & edit content based on the HOA Trustees' vision and needs, and remove outdated information. Make the website more visually appealing and user friendly." 
+    description: "Improved a WordPress-based HOA site with custom HTML/CSS for better visuals, usability, and up-to-date content. Built a fee calculator that shifted costs to customers, saving the HOA money." 
   }, {
-    id: 4,
+    id: 5,
     image: image2,
     name: "Let's GO!",
     link: 'https://scriptsculptorjs.github.io/lets-go/',
     skills: 'HTML5 | CSS | Javascript',
-    description: "Practice using the skills I have acquired from a SuperSimpleDev course on Youtube to create an app that makes GET requests from Bored's API and stores data in localStorage.",
+    description: "Fetches activity ideas via GET requests from the Bored API and stores them in localStorage, built using skills from a SuperSimpleDev course.",
     repoLink: 'https://github.com/ScriptSculptorJS/lets-go'
   }]
 
